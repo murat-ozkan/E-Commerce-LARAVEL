b7668767e77c1f04e9690aeea72a79a86ca054a0
muratozkan79 <muratozkan79@gmail.com>
2023-11-16 00:03:44 +0300

    Slider content

--- app/Http/Controllers/Frontend/PageHomeController.php
+++ app/Http/Controllers/Frontend/PageHomeController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-
+use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class PageHomeController extends Controller
 {
     public function home()
     {
-        return view('frontend.pages.index');
+        $slider = Slider::where('status', '1')->first(); // get kullanıldığında collection olur. o yüzden first yaptım.
+        return view('frontend.pages.index', compact('slider'));
     }
 }
