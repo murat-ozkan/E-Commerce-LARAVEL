b137eb60066f215b97ee3f7cc5460ca6836d9457
muratozkan79 <muratozkan79@gmail.com>
2023-11-20 08:27:34 +0300

    Contact Data Validation

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -2,15 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContentFormRequest;
 use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class AjaxController extends Controller
 {
-    public function createcontact(Request $request)
+    public function createcontact(ContentFormRequest $request) //? Request yerine artık ContentFormRequest çalışacak
     {
+        //? ******** Bu kısmı ContentFormRequest dosyasına taşıdık. Görüntü kirliliği olmasın diye.
+        // $validatedData = $request->validate([
+        //     'name' => 'required|string|min:3',
+        //     'email' => 'required|email',
+        //     'subject' => 'required',
+        //     'message' => 'required'
+        // ], [
+        //     'name.min' => 'Must be 3 chars at least',
+        //     'subject.required' => 'Subject can not be empty' //! Bu şekilde özel mesajlar alınabilir.
+        // ]);
+        //? ******** Bu kısmı ContentFormRequest dosyasına taşıdık. Görüntü kirliliği olmasın diye.
+
+
+
         $data = $request->all();
         $data['ip'] = $request->ip();
+
         $newRecord = Contact::create($data);
         return back()->withSuccess('data successfully recorded');
         // return back()->with(['message'=>'data successfully recorded']);
--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -4,7 +4,7 @@
     <div class="bg-light py-3">
         <div class="container">
             <div class="row">
-                <div class="col-md-12 mb-0"><a href="index.html">Home</a> <span class="mx-2 mb-0">/</span> <strong
+                <div class="col-md-12 mb-0"><a href="{{ route('home') }}">Home</a> <span class="mx-2 mb-0">/</span> <strong
                         class="text-black">Contact</strong></div>
             </div>
         </div>
@@ -19,6 +19,11 @@
                 <div class="col-md-7">
                     @if (session()->has('success'))
                         <div class="alert alert-success">{{ session()->get('success') }}</div>
+                    @endif
+                    @if (count($errors))
+                        @foreach ($errors->all() as $error)
+                            <div class="alert alert-success">{{ $error }}</div>
+                        @endforeach
                     @endif
                     <form action="{{ route('create.contact') }}" method="post">
                         @csrf
@@ -39,14 +44,16 @@
                             </div>
                             <div class="form-group row">
                                 <div class="col-md-12">
-                                    <label for="c_subject" class="text-black">Subject </label>
+                                    <label for="c_subject" class="text-black">Subject <span
+                                            class="text-danger">*</span></label>
                                     <input type="text" class="form-control" id="c_subject" name="subject">
                                 </div>
                             </div>
 
                             <div class="form-group row">
                                 <div class="col-md-12">
-                                    <label for="c_message" class="text-black">Message </label>
+                                    <label for="c_message" class="text-black">Message <span
+                                            class="text-danger">*</span></label>
                                     <textarea name="message" id="c_message" cols="30" rows="7" class="form-control"></textarea>
                                 </div>
                             </div>
